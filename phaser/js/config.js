102cb96abb85860fd7c37260e1b84463173b6d98
Divik <reastw1234@gmail.com>
2017-11-16 20:29:02 +0800

    ==Moving events and setup completed==

--- phaser/js/config.js
+++ phaser/js/config.js
@@ -1,12 +1,36 @@
 let Config = {
-  window:{
-    height: '100%',
-    width: '100%'
-  },
-  frame:{
-    leftFrame: 0,
-    rightFrame: 4
-  }
-
-
+    window:{
+        // type stored in string
+        width: '100%',
+        height: '100%'
+    },
+    frame:{
+        leftFrame: 0,
+        rightFrame: 4
+    },
+    velocity:{
+        left:-200,
+        right: 200,
+        up: -600,
+        gravity: 20,
+        idle: 0
+    },
+    picture:{
+        mario:{
+            width: 32,
+            height: 56
+        },
+        background:{
+            width:8000,
+            height:600,
+            x:0,
+            y:0
+        }
+    },
+    animation:{
+        left:[0,1,2,3],
+        idle:[4],
+        right:[4,5,6,7],
+        frameRate:10
+    }
 }
--- phaser/js/draw.js
+++ phaser/js/draw.js
@@ -1,161 +1,216 @@
 let Game = new Phaser.Game(
-  Config.window.width,
-  Config.window.height,
-  Phaser.CANVAS,
-  'demo',
-  {
+    Config.window.width,
+    Config.window.height,
+    Phaser.CANVAS,
+    'demo',
+    {
     preload: preload,
     create: create,
     update: update,
     render: render
-  });
+    }
+);
 let map;
 let layer;
 let players = {};
 let bg;
 
-function playerSetup(playerName, x=0, y=0)
+function playerSetup(playerName, x=0, y=0, controlable=false)
 {
-  function Player(character, cursor, x=0, y=0){
-    this.character = character;
-    this.cursor = cursor;
-    this.action = {
-      facing: 'idle',
-      attack: 'none'
-    };
-    this.x = x;
-    this.y = y;
-  }
+    function Player(character, cursor, x=0, y=0){
+        this.character = character;
+        this.cursor = cursor;
+        this.action = {
+              facing: 'idle',
+              attack: 'none'
+        };
+        this.x = x;
+        this.y = y;
+    }
 
-  let character = Game.add.sprite(x, y, 'player');
-  let cursor = Game.input.keyboard.createCursorKeys();
-  Game.physics.enable(character);
-  //character.body.linearDamping = 1;
-  character.body.collideWorldBounds = true;
-  // set up animations by Phaser engine
-  character.animations.add('left', [0, 1, 2, 3], 10, true);
-  character.animations.add('idle', [4], 20, true);
-  character.animations.add('right', [4, 5, 6, 7], 10, true);
-  players[playerName] = new Player(character, cursor, x, y);
+    function SyncCursor(){
+        this.up = {isDown: false};
+        this.down = {isDown: false};
+        this.left = {isDown: false};
+        this.right = {isDown: false};
+    }
+
+    let character = Game.add.sprite(x, y, 'player');
+    let cursor;
+    if(controlable)
+        cursor = Game.input.keyboard.createCursorKeys();
+    else
+        cursor = new SyncCursor();
+    Game.physics.enable(character);
+    character.body.collideWorldBounds = true;
+    // set up animations by Phaser engine
+    // add(name, frames, frameRate fps, loop)
+    character.animations.add('left', Config.animation.left, Config.animation.frameRate, true);
+    character.animations.add('idle', Config.animation.idle, Config.animation.frameRate, true);
+    character.animations.add('right', Config.animation.right, Config.animation.frameRate, true);
+    players[playerName] = new Player(character, cursor, x, y);
 }
 
 // load image and tilemap
 function preload()
 {
-    Game.load.tilemap('mario', 'phaser/assets/map1.json', null, Phaser.Tilemap.TILED_JSON);
-    Game.load.image('tileset', 'phaser/assets/tileset_x32.png');
-    Game.load.image('bg','phaser/assets/01.png');
-    Game.load.spritesheet('player', 'phaser/assets/mariox32.png',32,56);
+    Game.load.tilemap(
+        'mario',
+        'phaser/assets/map.json',
+        null,
+        Phaser.Tilemap.TILED_JSON);
+    Game.load.image(
+        'tileset',
+        'phaser/assets/tileset_x32.png');
+    Game.load.image(
+        'bg',
+        'phaser/assets/nature.png');
+    Game.load.spritesheet(
+        'player',
+        'phaser/assets/mariox32.png',
+        Config.picture.mario.width,
+        Config.picture.mario.height);
 }
 
 function create()
 {
     Game.physics.startSystem(Phaser.Physics.ARCADE);
-    bg = Game.add.tileSprite(0,0,8000,600,'bg');
+    bg = Game.add.tileSprite(
+        Config.picture.background.x,
+        Config.picture.background.y,
+        Config.picture.background.width,
+        Config.picture.background.height,
+        'bg');
     bg.fixedToCamera = true;
     map = Game.add.tilemap('mario');
-    layer = map.createLayer('tile');
+    layer = map.createLayer('World1');
     layer.resizeWorld();
     map.addTilesetImage('World', 'tileset');
-    map.setCollisionBetween(1,6);
-    map.setCollisionBetween(9,14);
-    map.setCollisionBetween(17,22);
-    map.setCollisionBetween(25,29);
-    map.setCollisionBetween(33,38);
-    playerSetup('self');
+    map.setCollisionByExclusion(map.tilesets[0].tileProperties.collisionExclusion);
+    playerSetup('self', 0, 0, true);
     Game.camera.follow(players.self.character);
+
+    /* debug */
+    playerSetup('Alice',100,20);
 }
 
 function update()
 {
-  for(let name in players)
-  {
-
-      let character = players[name].character;
-      let cursor = players[name].cursor;
-      let action = players[name].action;
-      let velocity = character.body.velocity;
-
-      Game.physics.arcade.collide(character, layer);
-      // stop moving to left or right
-      velocity.x = 0;
-      if (cursor.up.isDown)
-      {
-        if (character.body.onFloor()) velocity.y = -600;
-      }
-      if(!character.body.onFloor()) velocity.y += 20;
-      if(cursor.left.isDown)
-      {
-        velocity.x = -200;
-        if (action.facing != 'left')
-        {
-          character.animations.play('left');
-          action.facing = 'left';
-        }
-      }
-      else if (cursor.right.isDown)
-      {
-        velocity.x = 200;
-        if (action.facing != 'right')
-        {
-          character.animations.play('right');
-          action.facing = 'right';
-        }
-      else
-      {
-          character.animations.stop();
-          if (action.facing == 'left') character.frame = Config.frame.leftFrame;
-          else character.frame = Config.frame.rightFrame;
-          action.facing = 'idle';
-      }
-    }
-  }
-  /*
-
-  player.body.velocity.x=0;
-  if (cursors.up.isDown)
-  {
-    if (player.body.onFloor()) player.body.velocity.y = -600;
-  }
-  if(!player.body.onFloor()) player.body.velocity.y+=20;
-
-  if (cursors.left.isDown)
-  {
-
-  }
-  else if (cursors.right.isDown)
-  {
-    player.body.velocity.x = 200;
-    if (facing != 'right')
+    for(let name in players)
     {
-      player.animations.play('right');
-      facing = 'right';
+        let character = players[name].character;
+        Game.physics.arcade.collide(character, layer);
+        for(let other in players)
+        {
+            let otherCharacter = players[other].character;
+            Game.physics.arcade.collide(character, otherCharacter);
+        }
     }
-  }
-  else
-  {
-    if (facing != 'idle')
+    //console.log(map.tilesets[0].tileProperties[1]);
+    for(let name in players)
     {
-      player.animations.stop();
-      if (facing == 'left') player.frame = 0;
-      else player.frame = 4;
-      facing = 'idle';
-    }
-  }
-  */
 
+        let character = players[name].character;
+        let cursor = players[name].cursor;
+        let action = players[name].action;
+        let velocity = character.body.velocity;
+
+        // stop moving to left or right
+        velocity.x = Config.velocity.idle;
+        if (cursor.up.isDown)
+        {
+            //cursor.up.isDown = false;
+            if (character.body.onFloor()) velocity.y = Config.velocity.up;
+        }
+        if(!character.body.onFloor()) velocity.y += Config.velocity.gravity;
+        if(cursor.left.isDown)
+        {
+            //cursor.left.isDown = false;
+            velocity.x = Config.velocity.left;
+            if(action.facing != 'left')
+            {
+                character.animations.play('left');
+                action.facing = 'left';
+            }
+        }
+        else if (cursor.right.isDown)
+        {
+            //cursor.right.isDown = false;
+            velocity.x = Config.velocity.right;
+            if (action.facing != 'right')
+            {
+                character.animations.play('right');
+                action.facing = 'right';
+            }
+        }
+        else if (cursor.down.isDown)
+        {
+            //cursor.down.isDown = false; 
+            /*temperary unusable */
+        }
+        else
+        {
+            character.animations.stop();
+            if (action.facing == 'left') character.frame = Config.frame.leftFrame;
+            else character.frame = Config.frame.rightFrame;
+            action.facing = 'idle';
+        }
+    }
 }
 
 function render()
 {
 }
 
-window.addEventListener("keydown",function(e){
-  switch (e.which)
-  {
-    case 71:  //key 'g'
-    console.log(e.which);
-      playerSetup('Alice',10,20);
-      break;
-  }
+
+
+window.addEventListener("keypress",function(e){
+    switch (e.key)
+    {
+        /* spawn event
+        case 'g':
+            playerSetup('Alice',10,20);
+            fuck++;
+            break;
+        */
+
+        case 'w':
+            players.Alice.cursor.up.isDown = true;
+            break;
+        case 'a':
+            players.Alice.cursor.left.isDown = true;
+            break;
+        case 's':
+            players.Alice.cursor.down.isDown = true;
+            break;
+        case 'd':
+            players.Alice.cursor.right.isDown = true;
+            break;
+         
+    }
+});
+
+window.addEventListener("keyup", function(e){
+    switch (e.key)
+    {
+        /* spawn event
+        case 'g':
+            playerSetup('Alice',10,20);
+            fuck++;
+            break;
+        */
+
+        case 'w':
+            players.Alice.cursor.up.isDown = false;
+            break;
+        case 'a':
+            players.Alice.cursor.left.isDown = false;
+            break;
+        case 's':
+            players.Alice.cursor.down.isDown = false;
+            break;
+        case 'd':
+            players.Alice.cursor.right.isDown = false;
+            break;
+    }
 });
