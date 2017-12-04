8425c662d7fc58454422bff5a1e4c1722d4bf4b5
kaeteyaruyo <kaeteyaruyo@gmail.com>
2017-12-05 04:27:59 +0800

    login css

---
+++ public/game/js/regist.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    $('.box').hide().fadeIn(1000);
+    
+    $("form").submit(function(e){
+        if($("#account").val() == "" && $("#password").val() == ""){
+            e.preventDefault();
+            window.alert("Please enter your user name and password.");
+            return false;
+        }
+        else if($("#password").val() == ""){
+            e.preventDefault();
+            window.alert("Please enter your password.");
+            return false;
+        }
+        else if($("#account").val() == ""){
+            e.preventDefault();
+            window.alert("Please enter your user name.");
+            return false;
+        };
+    });
+})
