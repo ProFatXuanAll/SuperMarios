8e6e201f851149ae361790a6111ef3ee5e2167a2
kaeteyaruyo <kaeteyaruyo@gmail.com>
2017-12-09 21:59:52 +0800

    add naming page

--- routes/game.js
+++ routes/game.js
@@ -5,18 +5,28 @@ const session = require('express-session');
 const io = require("./socket")(app.server);
 const router = express.Router();
 
-/*
 app.use('/game',session({
   secret : 'dkwqhnfqwohfwnfuqha',
   resave : false,
   saveUninitialized : true,
 }));
-*/
 
 router.get('/', UrlSetting, function(req, res, next) {
-    res.render('game/index');
+    var sess = req.session;
+    if(sess.userName != undefined ) res.render('game/index',{ name : sess.userName });
+    else res.redirect('/game/name');
+});
+    
+router.get('/name', UrlSetting,function(req,res,next){
+    res.render('game/name');
 });
 
+router.post('/name', UrlSetting,function(req,res,next){
+    var sess = req.session;
+    sess.userName = req.body.account;
+    res.redirect('/game');
+});
+    
 router.get('/pedia', UrlSetting, function(req, res, next) {
     res.render('game/pedia');
 });
