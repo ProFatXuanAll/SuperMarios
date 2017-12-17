f489515f1445647d1e5a91133a6dd9c4a62551ff
kevin <ProFatXuanAll@gmail.com>
2017-12-18 00:15:20 +0800

    server side refresh crash solved

--- routes/socket.js
+++ routes/socket.js
@@ -8,7 +8,7 @@ module.exports = function(server){
         // new player tell server to join game
         socket.on('00 playerJoin', function(playerData){
             
-            // if someone open multiple tabs
+            // if player open multiple tabs
             if(playerList[playerData.name])
             {
                 socket.emit('multipleConnection');
@@ -17,7 +17,7 @@ module.exports = function(server){
 
             console.log(playerData.name + ' join');
             
-            // server tell existed player(s) info new of player
+            // server tell existed player(s) info of new player
             socket.broadcast.emit(
                 '01 toExistPlayer',
                 playerData
@@ -26,8 +26,6 @@ module.exports = function(server){
 
             // server store new player info
             playerList[playerData.name] = {
-                // data about player
-                data: playerData,
                 // socket between server and player
                 socket: socket
             };
@@ -41,20 +39,26 @@ module.exports = function(server){
             }
         });
         
-        // server tell new player info of exist player(s)
+        // existed player(s) tell server its info
         socket.on('02 toNewPlayer', function(playerData){
-            playerList[playerData.requestName].socket.emit(
-                '02 toNewPlayer',
-                playerData.data
-            );
+            // if new player not disconnect
+            if(playerList[playerData.requestName])
+            {
+                // server tell new player info of exist player(s)
+                playerList[playerData.requestName].socket.emit(
+                    '02 toNewPlayer',
+                    playerData.data
+                );
+            }
         });
         
-        // new player tell server to get monster
+        // new player tell server to get monster info
         socket.on('04 requestMonster', function(playerData){
-            // find exist user to sync monster
+            // new player be super user if super does not exist
             if(superUser == null)
             {
                 superUser = playerData.name;
+                // server tell new player to generate monster by itself
                 socket.emit(
                      '07 spawnMonster',
                      {
@@ -62,17 +66,21 @@ module.exports = function(server){
                      }
                 );
             }
+            // new player is super user
             else if(superUser == socket.username)
             {
-               socket.emit(
+                // server tell new player to generate monster by itself
+                socket.emit(
                     '07 spawnMonster',
                     {
-                       superUser: true
+                        superUser: true
                     }
-               );
+                );
             }
+            // new player is not super user
             else
             {
+                // server tell super user to return monster info
                 playerList[superUser].socket.emit(
                     '05 getMonsterInfo',
                     playerData
@@ -80,23 +88,29 @@ module.exports = function(server){
             }
         });
 
-        // parsing item info
+        // super user tell server monster info
         socket.on('06 parseMonsterInfo', function(monsterData){
-            playerList[monsterData.requestName].socket.emit(
-                '07 spawnMonster',
-                {
-                    superUser: false,
-                    monsterGroup: monsterData.monsterGroup
-                }
-            );
+            // if new player not disconnect
+            if(playerList[monsterData.requestName])
+            {
+                // server tell new player to parse monster info
+                playerList[monsterData.requestName].socket.emit(
+                    '07 spawnMonster',
+                    {
+                        superUser: false,
+                        monsterGroup: monsterData.monsterGroup
+                    }
+                );
+            }
         });
 
-        // new player tell server to get item
+        // new player tell server to get item info
         socket.on('08 requestItem', function(playerData){
-            // find exist user to sync item
+            // new player be super user if super does not exist
             if(superUser == null)
             {
                 superUser = playerData.name;
+                // server tell new player to generate item by itself
                 socket.emit(
                     '11 spawnItem',
                     {
@@ -104,8 +118,10 @@ module.exports = function(server){
                     }
                 );
             }
+            // new player is not super user
             else if(superUser == socket.username)
             {
+                // server tell new player to generate item by itself
                 socket.emit(
                     '11 spawnItem',
                     {
@@ -113,8 +129,10 @@ module.exports = function(server){
                     }
                 );
             }
+            // new player is not super user
             else
             {
+                // server tell super user to return item info
                 playerList[superUser].socket.emit(
                     '09 getItemInfo',
                     playerData
@@ -122,30 +140,23 @@ module.exports = function(server){
             }
         });
 
-        // parsing item info
+        // super user tell server item info
         socket.on('10 parseItemInfo', function(itemData){
-            playerList[itemData.requestName].socket.emit(
-                '11 spawnItem',
-                {
-                    superUser: false,
-                    itemGroup: itemData.itemGroup
-                }
-            );
+            // if new player not disconnect
+            if(playerList[itemData.requestName])
+            {
+                // server tell new player to parse item info
+                playerList[itemData.requestName].socket.emit(
+                    '11 spawnItem',
+                    {
+                        superUser: false,
+                        itemGroup: itemData.itemGroup
+                    }
+                );
+            }
         });
 
-        socket.on('playerMove',function(playerData){
-            socket.broadcast.emit(
-                'playerMove',
-                playerData);
-        });
-
-        socket.on('playerStop',function(playerData){
-            socket.broadcast.emit(
-                'playerStop',
-                playerData);
-        });
-
-        // someone disconnect
+        // player disconnect
         socket.on('disconnect', function(){
             if(socket.username == superUser)
             {
@@ -163,69 +174,99 @@ module.exports = function(server){
                     superUser = null;
             }
 
-            delete playerList[socket.username];
             // delete player from player list
+            delete playerList[socket.username];
+            // server tell everyone to delete player
             socket.broadcast.emit(
                 'playerDelete',
                 {
-                    name:socket.username
+                    name: socket.username
                 }
             );
         });
 
-        //someone died
+        // existed player(s) tell server it move
+        socket.on('playerMove',function(playerData){
+            // server tell other player(s) it move
+            socket.broadcast.emit(
+                'playerMove',
+                playerData);
+        });
+
+        // existed player(s) tell server it stop
+        socket.on('playerStop',function(playerData){
+            // server tell other player(s) it stop
+            socket.broadcast.emit(
+                'playerStop',
+                playerData);
+        });
+
+        // existed player(s) tell server it dead
         socket.on('playerDead', function(playerData){
+            // server tell everyone it dead
             io.emit(
                 'playerDead',
                 playerData
             );
         });
 
+        // existed player(s) tell server it respawn
         socket.on('playerRespawn', function(playerData){
+            // server tell everyone it respawn
             io.emit(
                 'playerRespawn',
                 playerData
             );
         });
 
+        // existed player(s) tell server it hit middle point
         socket.on('playerMidpoint', function(playerData){
+            // server tell everyone it hit middle point
             io.emit(
                 'playerMidpoint',
                 playerData
             );
         });
 
+        // existed player(s) tell server it hit finish point
         socket.on('playerFinish', function(playerData){
+            // server tell everyone it hit finish point
             io.emit(
                 'playerFinish',
                 playerData
             );
         });
 
-        // some monster dead
+        // existed player(s) tell server it kill monster
         socket.on('monsterDead', function(monsterData){
+            // server tell everyone it kill monster
             io.emit(
                 'monsterDead',
                 monsterData
             );
         });
 
+        // existed player(s) tell server it respawn monster
         socket.on('monsterRespawn', function(monsterData){
+            // server tell everyone it respawn monster
             io.emit(
                 'monsterRespawn',
                 monsterData
             );
         });
 
-        // some item dead
+        // existed player(s) tell server it kill item
         socket.on('itemDead', function(itemData){
+            // server tell everyone it kill item
             io.emit(
                 'itemDead',
                 itemData
             );
         });
 
+        // existed player(s) tell server it respawn item
         socket.on('itemRespawn', function(itemData){
+            // server tell everyone it respawn item
             io.emit(
                 'itemRespawn',
                 itemData
