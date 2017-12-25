9dfbe2d981cce523e5ee868a7728638b9833e888
kaeteyaruyo <kaeteyaruyo@gmail.com>
2017-12-26 04:49:11 +0800

    game.js lint and add summary page

--- routes/game.js
+++ routes/game.js
@@ -6,47 +6,52 @@ const io = require("./socket")(app.server);
 const router = express.Router();
 
 app.use('/game',session({
-  secret : 'dkwqhnfqwohfwnfuqha',
-  resave : false,
+  secret: 'dkwqhnfqwohfwnfuqha',
+  resave: false,
   saveUninitialized : true,
 }));
 
-router.get('/', UrlSetting, function(req, res, next) {
-    var sess = req.session;
+router.get('/', UrlSetting, function(req, res, next){
+    let sess = req.session;
     if(sess.userName === undefined)
     {
         res.redirect('/game/name');
     }
     else
     {
-        res.render('game/index',{ name : sess.userName });
+        res.render('game/index', { name: sess.userName });
     }
 });
     
-router.get('/name', UrlSetting,function(req,res,next){
-    var sess = req.session;
+router.get('/name', UrlSetting, function(req, res, next){
+    let sess = req.session;
     if(sess.userName)
     {
+	//this part should be remove before project release
         res.send('You should not see this page.');
     }
     else
     {
-        res.render('game/name',{ error: false });
+        res.render('game/name', { error: false });
     }
 });
 
-router.post('/name', UrlSetting,function(req,res,next){
-    var sess = req.session;
+router.post('/name', UrlSetting, function(req, res, next){
+    let sess = req.session;
     if(io.playerList[req.body.account])
     {
-        res.render('game/name',{ error: true });
+        res.render('game/name', { error: true });
     }
     else
     {
-        console.log(req.body.account);
         sess.userName = req.body.account;
         res.redirect('/game');
     }
+});
+
+router.get('/summary', UrlSetting, function(req, res, next){
+    //there should be an object "summary" in socket.js, which include sorted players information
+    res.render('game/summary', { players: io.summary });
 });
 
 router.get('/pedia', UrlSetting, function(req, res, next){
@@ -54,10 +59,10 @@ router.get('/pedia', UrlSetting, function(req, res, next){
 });
 
 router.get('/error', UrlSetting, function(req, res, next){
-    res.render('game/error',{ error: 'multipleConnect' });
+    res.render('game/error', { error: 'multipleConnect' });
 });
 
-function UrlSetting(req,res,next){
+function UrlSetting(req, res, next){
     res.locals = {
         title: 'SuperMarios',
         home: '/home',
