66e63b8c20677ba1a10f0346512093ca39f0d667
kaeteyaruyo <kaeteyaruyo@gmail.com>
2018-01-06 17:24:51 +0800

    working on login.js

--- public/game/js/login.js
+++ public/game/js/login.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    let state = 0;
+    let totalState = 2;
+
     $("form").submit(function(e){
         if($("#account").val() == ""){
         e.preventDefault();
@@ -7,9 +10,36 @@ $(document).ready(function(){
         }
         else
         {
-            var name = escape($("#account").val());
+            var name = htmlEntities($("#account").val());
             $("#account").val(name);
             return true;
         }
     });
+
+    document.getElementById("previous-btn").addEventListener("click", () => { changeState(false) });
+    document.getElementById("next-btn").addEventListener('click', () => { changeState(true) });
+
+	function htmlEntities(str) {
+    	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+	}
+
+    function changeState(toRight){
+        if( toRight == true && state < totalState-1 ) ++state;
+        if( toRight == false && state > 0 ) --state;
+        setStateTitle();
+    }
+
+    function setStateTitle(){
+        let stateTitle = document.getElementsByTagName("h1")[0];
+        if( state === 0 ) stateTitle.innerHTML = "What's your name?";
+        else if( state === 1 ) stateTitle.innerHTML = "Choose a character";
+    }
+
+    function setForm(){
+        let formContent = document.getElementsByTagName("form")[0].getElementsByTagName("section");
+        for(let i = 0; i < formContent.length; ++i){
+            formContent[i].style.display = "none";
+        }
+        formContent[state].style.display = "block";
+    }
 });
